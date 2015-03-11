8ac06e500b1e5cadcda7d0136e441301455f205a
Stu Kabakoff <stuart@compstak.com>
2015-03-11 18:44:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,171 @@
+var http = require('http');
+
+var sameHandCost = 2;
+var sameFingerCost = 4;
+var sameKeyCost = 1;
+
+var keyCost = {
+	a: 0,
+	b: 2,
+	c: 1,
+	d: 0,
+	e: 1,
+	f: 0,
+	g: 1,
+	h: 1,
+	i: 1,
+	j: 0,
+	k: 0,
+	l: 0,
+	m: 1,
+	n: 1,
+	o: 1,
+	p: 1,
+	q: 2,
+	r: 1,
+	s: 0,
+	t: 2,
+	u: 1,
+	v: 1,
+	w: 1,
+	x: 1,
+	y: 2,
+	z: 1,
+};
+
+var hand = {
+	a: 1,
+	b: 1,
+	c: 1,
+	d: 1,
+	e: 1,
+	f: 1,
+	g: 1,
+	h: 2,
+	i: 2,
+	j: 2,
+	k: 2,
+	l: 2,
+	m: 2,
+	n: 2,
+	o: 2,
+	p: 2,
+	q: 1,
+	r: 1,
+	s: 1,
+	t: 1,
+	u: 2,
+	v: 1,
+	w: 1,
+	x: 1,
+	y: 2,
+	z: 1,
+};
+
+var finger = {
+	a: 1,
+	b: 6,
+	c: 3,
+	d: 3,
+	e: 3,
+	f: 4,
+	g: 4,
+	h: 5,
+	i: 6,
+	j: 5,
+	k: 5,
+	l: 7,
+	m: 5,
+	n: 5,
+	o: 7,
+	p: 7,
+	q: 2,
+	r: 4,
+	s: 2,
+	t: 4,
+	u: 5,
+	v: 4,
+	w: 2,
+	x: 3,
+	y: 5,
+	z: 2,
+};
+
+function calculateScore (word) {
+	var letters = word.toLowerCase().split('');
+
+	var letterCost = letters.reduce(function (score, letter) {
+		return score + keyCost[letter];
+	}, 0);
+
+	var handCost = letters.reduce(function (score, letter, i) {
+		if (hand[letter] === hand[letters[i+1]]) {
+			return score + sameHandCost;
+		} else {
+			return score;
+		}
+	}, 0);
+
+	var fingerCost = letters.reduce(function (score, letter, i) {
+		if (letter === letters[i+1]) {
+			return score + sameKeyCost;
+		} else if (finger[letter] === finger[letters[i+1]]) {
+			return score + sameFingerCost;
+		} else {
+			return score;
+		}
+	}, 0);
+
+	var total = letterCost + handCost + fingerCost + Math.floor(word.length);
+
+	return total;
+}
+
+http.get('http://words.bighugelabs.com/api/2/3adad4676426edc5a0b3c7860bb4866c/service/json', function (res) {
+	res.on('data', function (data) {
+		data = JSON.parse(data.toString());
+
+		var types = Object.keys(data);
+
+		var results = [];
+
+		types.forEach(function (type) {
+			data[type].syn.filter(function (word) {
+				return word.indexOf(' ') === -1;
+			}).forEach(function (word) {
+				var score = calculateScore(word);
+				results.push({
+					word: word,
+					score: score
+				});
+			});
+		});
+
+		results.sort(function (a, b) {
+			return a.score - b.score;
+		});
+
+		results.forEach(function (result) {
+			console.log( result.score +' '+ result.word );
+		});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Word Cost
+
+This tool calculates how hard it is to type a word. It takes into account how far your fingers need to move to type a key, which hand is used to type a letter, how long words are, and repeated letters.
+
+Right now the word "service" is hard coded, but it may be expanded.
+
+
+Ideally it would use the scoring system from this paper:
+http://web.mst.edu/~tauritzd/courses/ec/fs2003/project/Walker.pdf
